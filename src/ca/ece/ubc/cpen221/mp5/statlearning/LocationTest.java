d2d61ac2826ffc2b90899b7032da5239ac13d7f1
Cinnie <cmusicfreak@Cinnies-MacBook-Pro.local>
2015-12-08 01:44:43 -0800

    whoops forgot i deleted a method

--- src/ca/ece/ubc/cpen221/mp5/statlearning/LocationTest.java
+++ src/ca/ece/ubc/cpen221/mp5/statlearning/LocationTest.java
@@ -10,46 +10,6 @@ import ca.ece.ubc.cpen221.mp5.Restaurant;
 import ca.ece.ubc.cpen221.mp5.server.RestaurantDB;
 
 public class LocationTest {
-
-	@Test
-	public void testGetRandomLocations1() {
-		List<Location> test = Location.getRandomLocations(5,
-				new RestaurantDB("data/restaurants.json", "data/reviews.json", "data/users.json"));
-		System.out.println(test.toString());
-		assertEquals(test.size(), 5);
-	}
-
-	@Test
-	public void testGetRandomLocations2() {
-		List<Location> test = Location.getRandomLocations(135,
-				new RestaurantDB("data/restaurants.json", "data/reviews.json", "data/users.json"));
-		System.out.println(test.toString());
-		assertEquals(test.size(), 135);
-	}
-	
-	@Test
-	public void testGetRandomLocations3() {
-		List<Location> test = Location.getRandomLocations(119,
-				new RestaurantDB("data/restaurants.json", "data/reviews.json", "data/users.json"));
-		System.out.println(test.toString());
-		assertEquals(test.size(), 119);
-	}
-	
-	@Test
-	public void testGetRandomLocations4() {
-		List<Location> test = Location.getRandomLocations(120,
-				new RestaurantDB("data/restaurants.json", "data/reviews.json", "data/users.json"));
-		System.out.println(test.toString());
-		assertEquals(test.size(), 120);
-	}
-	
-	@Test
-	public void testGetRandomLocations5() {
-		List<Location> test = Location.getRandomLocations(121,
-				new RestaurantDB("data/restaurants.json", "data/reviews.json", "data/users.json"));
-		System.out.println(test.toString());
-		assertEquals(test.size(), 121);
-	}
 	
 	@Test
 	public void testGetDistance() {
